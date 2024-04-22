9b3821953dc89a9c3769e2d13d80701693efd48a
Farok Hossain <farokafs7@gmail.com>
2024-04-22 19:19:13 +0600

    image added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["encrypted-tbn2.gstatic.com"],
+  },
+};
 
 export default nextConfig;
---
+++ src/app/album/page.js
@@ -0,0 +1,27 @@
+import Image from "next/image";
+// import myImage from "../../assets/images/myImage.jpg";
+import myImage from "@/assets/images/myImage.jpg";
+
+const AlbumPage = () => {
+  return (
+    <div>
+      <h1>Using image component</h1>
+      <Image
+        src="https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcQ9kXiqxWGfYyCJphYUSq8-H2typSIwHmxojTDAMIgqU-BhhKQkr_hELs5KEKQRdctSxHwGBbhg9w8n8PU"
+        width={800}
+        height={200}
+        alt="putin"
+      />
+      <Image src={myImage} width={800} height={200} alt="putin" />
+      <h1>Using img component</h1>
+      <img
+        src="https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcQ9kXiqxWGfYyCJphYUSq8-H2typSIwHmxojTDAMIgqU-BhhKQkr_hELs5KEKQRdctSxHwGBbhg9w8n8PU"
+        width="800px"
+        height="200px"
+        alt="putin"
+      />
+    </div>
+  );
+};
+
+export default AlbumPage;
