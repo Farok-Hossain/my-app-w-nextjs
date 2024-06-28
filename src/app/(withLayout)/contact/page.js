ce174d3c73a93226f0bf88cc19747da56fb2bb5b
Farok Hossain <farokafs7@gmail.com>
2024-06-28 21:56:35 +0600

    dashboard added

--- src/app/(withLayout)/contact/page.js
+++
@@ -1,9 +0,0 @@
-const ContactPage = () => {
-  return (
-    <div>
-      <h1>Contact page with header and footer</h1>
-    </div>
-  );
-};
-
-export default ContactPage;
---
+++ src/app/(withLayout)/dashboard/layout.js
@@ -0,0 +1,13 @@
+import Footer from "@/components/Shared/Footer";
+import React from "react";
+
+const DashboardLayout = ({ children }) => {
+  return (
+    <div>
+      <div className=" min-h-screen">{children}</div>
+      <Footer />
+    </div>
+  );
+};
+
+export default DashboardLayout;
