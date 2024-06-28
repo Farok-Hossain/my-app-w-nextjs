8c9fd09c30f7cdcb152b6da0a53f8ac20788922f
Farok Hossain <farokafs7@gmail.com>
2024-06-28 22:59:23 +0600

    about contact page added

---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+import styles from "./About.module.css";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1 className={styles.heading}>This is about page</h1>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ src/app/contact/page.js
@@ -0,0 +1,11 @@
+import styles from "./contact.module.css";
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1 className={styles.heading}>This is contact page</h1>
+    </div>
+  );
+};
+
+export default ContactPage;
